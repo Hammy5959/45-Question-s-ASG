c31856c42b3713e66d63e50e2fbfe8f0a8d1f52c
hamid <ziddisheikh60@gmail.com>
2024-01-23 01:19:29 +0500

    45 Question ASG

---
+++ dist/Q45.js
@@ -0,0 +1,16 @@
+"use strict";
+`Cars: Write a function that stores information about a car in a Object. The function should 
+always receive a manufacturer and a model name. It should then accept an arbitrary number of 
+keyword arguments. Call the function with the required information and two other name-value 
+pairs, such as a color or an optional feature. Print the Object that's returned to make sure
+ all the information was stored correctly.`;
+function createCar(manufacturer, modelName, options) {
+    const car = Object.assign({ manufacturer,
+        modelName }, options);
+    return car;
+}
+const myCar = createCar("Toyota", "Camry", {
+    color: "blue",
+    optionalFeature: "sunroof",
+});
+console.log(myCar);
